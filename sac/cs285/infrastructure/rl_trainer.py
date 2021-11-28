18b1c94f08b0c970b16352c45e3790dde6a422b7
adityabingi <adityabingi786@gmail.com>
2021-11-28 15:00:24 +0530

    soft-actor-critic algorithm

---
+++ sac/cs285/infrastructure/rl_trainer.py
@@ -0,0 +1,265 @@
+from collections import OrderedDict
+import pickle
+import os
+import sys
+import time
+
+import gym
+from gym import wrappers
+import numpy as np
+import torch
+from cs285.infrastructure import pytorch_util as ptu
+
+from cs285.infrastructure import utils
+from cs285.infrastructure.logger import Logger
+
+from cs285.agents import SACAgent
+
+# how many rollouts to save as videos to tensorboard
+MAX_NVIDEO = 2
+MAX_VIDEO_LEN = 40 # we overwrite this in the code below
+
+def get_wrapper_by_name(env, classname):
+    currentenv = env
+    while True:
+        if classname in currentenv.__class__.__name__:
+            return currentenv
+        elif isinstance(env, gym.Wrapper):
+            currentenv = currentenv.env
+        else:
+            raise ValueError("Couldn't find wrapper named %s"%classname)
+
+
+class RL_Trainer(object):
+
+    def __init__(self, params):
+
+        #############
+        ## INIT
+        #############
+
+        # Get params, create logger
+        self.params = params
+        self.logger = Logger(self.params['logdir'])
+
+        # Set random seeds
+        seed = self.params['seed']
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        ptu.init_gpu(
+            use_gpu=not self.params['no_gpu'],
+            gpu_id=self.params['which_gpu']
+        )
+
+        #############
+        ## ENV
+        #############
+
+        # Make the gym environment
+        self.env = gym.make(self.params['env_name'])
+        self.env = wrappers.Monitor(
+            self.env,
+            os.path.join(self.params['logdir'], "gym"),
+            force=True,
+            video_callable=(None if self.params['video_log_freq'] > 0 else False),
+        )
+        self.env.seed(seed) 
+
+        self.mean_episode_reward = -float('nan')
+        self.best_mean_episode_reward = -float('inf')
+
+        # import plotting (locally if 'obstacles' env)
+        if not(self.params['env_name']=='obstacles-cs285-v0'):
+            import matplotlib
+            matplotlib.use('Agg')
+
+        # Maximum length for episodes
+        self.params['ep_len'] = self.params['ep_len'] or self.env.spec.max_episode_steps
+        global MAX_VIDEO_LEN
+        MAX_VIDEO_LEN = self.params['ep_len']
+
+        # Is this env continuous, or self.discrete?
+        discrete = isinstance(self.env.action_space, gym.spaces.Discrete)
+        # Are the observations images?
+        img = len(self.env.observation_space.shape) > 2
+
+        self.params['agent_params']['discrete'] = discrete
+
+        # Observation and action sizes
+
+        ob_dim = self.env.observation_space.shape if img else self.env.observation_space.shape[0]
+        ac_dim = self.env.action_space.n if discrete else self.env.action_space.shape[0]
+        self.params['agent_params']['ac_dim'] = ac_dim
+        self.params['agent_params']['ob_dim'] = ob_dim
+
+        # simulation timestep, will be used for video saving
+        if 'model' in dir(self.env):
+            self.fps = 1/self.env.model.opt.timestep
+        elif 'env_wrappers' in self.params:
+            self.fps = 30 # This is not actually used when using the Monitor wrapper
+        elif 'video.frames_per_second' in self.env.env.metadata.keys():
+            self.fps = self.env.env.metadata['video.frames_per_second']
+        else:
+            self.fps = 10
+
+
+        #############
+        ## AGENT
+        #############
+
+        agent_class = self.params['agent_class']
+        self.agent = agent_class(self.env, self.params['train_batch_size'], self.params['agent_params'], self.params['restore_path'])
+
+    def run_training_loop(self, n_iter, collect_policy, eval_policy,
+                          initial_expertdata=None, relabel_with_expert=False,
+                          start_relabel_with_expert=1, expert_policy=None):
+        """
+        :param n_iter:  number of (dagger) iterations
+        :param collect_policy:
+        :param eval_policy:
+        :param initial_expertdata:
+        :param relabel_with_expert:  whether to perform dagger
+        :param start_relabel_with_expert: iteration at which to start relabel with expert
+        :param expert_policy:
+        """
+
+        # init vars at beginning of training
+        self.total_envsteps = self.agent.t
+        self.start_time = time.time()
+
+        for itr in range(self.agent.t, n_iter):
+            print("\n\n********** Iteration %i ************"%itr)
+
+            # decide if videos should be rendered/logged at this iteration
+            if itr % self.params['video_log_freq'] == 0 and self.params['video_log_freq'] != -1:
+                self.logvideo = True
+            else:
+                self.logvideo = False
+            self.log_video = self.logvideo
+
+            # decide if metrics should be logged
+            if self.params['scalar_log_freq'] == -1:
+                self.logmetrics = False
+            elif itr % self.params['scalar_log_freq'] == 0:
+                self.logmetrics = True
+            else:
+                self.logmetrics = False
+
+
+            if isinstance(self.agent, SACAgent):
+                # only perform an env step and add to replay buffer for DQN
+                self.agent.step_env()
+                envsteps_this_batch = 1
+                train_video_paths = None
+                paths = None
+            else:
+                use_batchsize = self.params['batch_size']
+                if itr==0:
+                    use_batchsize = self.params['batch_size_initial']
+                paths, envsteps_this_batch, train_video_paths = (
+                    self.collect_training_trajectories(
+                        itr, initial_expertdata, collect_policy, use_batchsize)
+                )
+
+            self.total_envsteps += envsteps_this_batch
+
+            # add collected data to replay buffer
+            #self.agent.add_to_replay_buffer(paths)
+            # train agent (using sampled data from replay buffer)
+            train_logs = self.train_agent()
+
+            # log/save
+            if self.logvideo or self.logmetrics:
+                # perform logging
+                print('\nBeginning logging procedure...')
+                self.perform_sac_logging(itr, eval_policy, train_logs)
+
+                if (itr % self.params['checkpoint_freq'] == 0) and self.params['save_params']:
+                    self.agent.save('{}/agent_itr_{}.pt'.format(self.params['logdir'], itr))
+
+    ####################################
+    ####################################
+
+    def collect_training_trajectories(self, itr, load_initial_expertdata, collect_policy, batch_size):
+        # TODO: get this from hw1
+        # if your load_initial_expertdata is None, then you need to collect new trajectories at *every* iteration
+        if itr == 0 and load_initial_expertdata is not None:
+            with open(load_initial_expertdata, 'rb') as f:
+                paths = pickle.load(f)
+            envsteps_this_batch = 0
+        else:
+            paths, envsteps_this_batch = utils.sample_trajectories(self.env, collect_policy, batch_size, self.params['ep_len'])
+
+        # collect more rollouts with the same policy, to be saved as videos in tensorboard
+        # note: here, we collect MAX_NVIDEO rollouts, each of length MAX_VIDEO_LEN
+        train_video_paths = None
+        if self.log_video:
+            print('\nCollecting train rollouts to be used for saving videos...')
+            ## TODO look in utils and implement sample_n_trajectories
+            train_video_paths = utils.sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
+
+        return paths, envsteps_this_batch, train_video_paths
+
+    def train_agent(self):
+
+        print('\nTraining agent using sampled data from replay buffer...')
+        train_logs = []
+        for train_step in range(self.params['num_agent_train_steps_per_iter']):
+
+            # TODO sample some data from the data buffer
+            # HINT1: use the agent's sample function
+            # HINT2: how much data = self.params['train_batch_size']
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.agent.sample(self.params['train_batch_size'])
+
+            # TODO use the sampled data to train an agent
+            # HINT: use the agent's train function
+            # HINT: keep the agent's training log for debugging
+            train_log = self.agent.train(ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch)
+            train_logs.append(train_log)
+
+        return train_logs
+
+    ####################################
+    ####################################
+
+    def perform_sac_logging(self, itr, eval_policy, all_logs):
+
+        last_log = all_logs[-1]
+
+        #######################
+        episode_rewards = get_wrapper_by_name(self.env, "Monitor").get_episode_rewards()
+        if len(episode_rewards) > 0:
+            self.mean_episode_reward = np.mean(episode_rewards[-10:])
+            self.episode_reward_std  = np.std(episode_rewards[-10:])
+        if len(episode_rewards) > 10:
+            self.best_mean_episode_reward = max(self.best_mean_episode_reward, self.mean_episode_reward)
+
+        logs = OrderedDict()
+
+        logs["Train_EnvstepsSoFar"] = self.agent.t
+        print("Timestep %d" % (self.agent.t,))
+
+        if self.mean_episode_reward > -5000:
+            logs["Train_AverageReturn"] = np.mean(self.mean_episode_reward)
+            logs["Train_StdReturn"]     = self.episode_reward_std
+        print("mean reward (10 episodes) %f" % self.mean_episode_reward)
+        if self.best_mean_episode_reward > -5000:
+            logs["Train_BestReturn"] = np.mean(self.best_mean_episode_reward)
+        print("best mean reward %f" % self.best_mean_episode_reward)
+
+        if self.start_time is not None:
+            time_since_start = (time.time() - self.start_time)
+            print("running time %f" % time_since_start)
+            logs["TimeSinceStart"] = time_since_start
+
+        logs.update(last_log)
+
+        sys.stdout.flush()
+
+        # perform the logging
+        for key, value in logs.items():
+            print('{} : {}'.format(key, value))
+            self.logger.log_scalar(value, key, self.agent.t)
+        print('Done logging...\n\n')
+
+        self.logger.flush()
---
+++ sac/cs285/policies/MLP_policy.py
@@ -0,0 +1,177 @@
+import abc
+import itertools
+from torch import nn
+from torch.nn import functional as F
+from torch import optim
+
+import numpy as np
+import torch
+from torch import distributions
+
+from cs285.infrastructure import pytorch_util as ptu
+from cs285.policies.base_policy import BasePolicy
+from cs285.infrastructure.utils import *
+
+LOG_STD_MIN = -20
+LOG_STD_MAX = 2
+
+def init_method(layer):
+    if isinstance(layer, nn.Linear):
+        nn.init.xavier_normal_(layer.weight)
+        nn.init.constant_(layer.bias, 0.0)
+
+
+class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
+
+    def __init__(self,
+                 ac_dim,
+                 ob_dim,
+                 n_layers,
+                 size,
+                 discrete=False,
+                 learning_rate=1e-4,
+                 training=True,
+                 nn_baseline=False,
+                 **kwargs
+                 ):
+        super().__init__(**kwargs)
+
+        # init vars
+        self.ac_dim = ac_dim
+        self.ob_dim = ob_dim
+        self.n_layers = n_layers
+        self.discrete = discrete
+        self.size = size
+        self.learning_rate = learning_rate
+        self.training = training
+        self.nn_baseline = nn_baseline
+
+        if self.discrete:
+            self.logits_na = ptu.build_mlp(input_size=self.ob_dim,
+                                           output_size=self.ac_dim,
+                                           n_layers=self.n_layers,
+                                           size=self.size)
+            self.logits_na.to(ptu.device)
+            self.mean_net = None
+            self.logstd = None
+            self.optimizer = optim.Adam(self.logits_na.parameters(),
+                                        self.learning_rate)
+        else:
+            self.logits_na = None
+            self.common= ptu.build_mlp(input_size=self.ob_dim,
+                                      output_size=self.size,
+                                      n_layers=1, size=self.size,
+                                      activation = 'relu',
+                                      output_activation='relu')
+
+            self.mean = nn.Linear(self.size, self.ac_dim)
+            self.logstd = nn.Linear(self.size, self.ac_dim)
+            self.common.to(ptu.device)
+            self.mean.to(ptu.device)
+            self.logstd.to(ptu.device)
+            self.optimizer = optim.Adam(
+                itertools.chain(self.common.parameters(), self.mean.parameters(), self.logstd.parameters()),
+                self.learning_rate
+            )
+
+        self.apply(init_method)
+    ##################################
+    ##################################
+
+    # query the policy with observation(s) to get selected action(s)
+    def get_action(self, obs: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
+
+    # update/train this policy
+    def update(self, observations, actions, **kwargs):
+        raise NotImplementedError
+
+    # This function defines the forward pass of the network.
+    # You can return anything you want, but you should be able to differentiate
+    # through it. For example, you can return a torch.FloatTensor. You can also
+    # return more flexible objects, such as a
+    # `torch.distributions.Distribution` object. It's up to you!
+    def forward(self, observation: torch.FloatTensor):
+        # TODO: get this from hw1
+        if self.discrete:
+            action_distribution = distributions.Categorical(logits = self.logits_na(observation))
+        else:
+            out = self.common(observation)
+            mean = self.mean(out)
+            logstd = self.logstd(out)
+            logstd = torch.clamp(logstd, LOG_STD_MIN, LOG_STD_MAX)
+            std = torch.exp(logstd)
+            action_distribution = distributions.Normal(loc=mean, scale=std)
+
+        return action_distribution
+
+
+    
+
+#####################################################
+#####################################################
+
+class SAC_Alpha_Policy(MLPPolicy):
+    def __init__(self, ac_dim, ob_dim, n_layers, size,
+                 action_low, action_high, **kwargs):
+
+        super().__init__(ac_dim, ob_dim, n_layers, size, **kwargs)
+        self.baseline_loss = nn.MSELoss()
+
+        self.action_scale = torch.FloatTensor((action_high -action_low)/2.0).to(ptu.device)
+        self.action_bias  = torch.FloatTensor((action_high + action_low)/2.0).to(ptu.device)
+
+    #################################
+
+    def policy_parameters(self):
+        if self.discrete:
+            return self.logits_na.parameters()
+
+        return itertools.chain(self.common.parameters(), self.mean.parameters(), self.logstd.parameters())
+
+    def get_action(self, obs: np.ndarray) -> np.ndarray:
+        
+        if len(obs.shape) > 1:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        if self.discrete:
+            action = self(ptu.from_numpy(observation)).sample()
+        else:
+            dist = self(ptu.from_numpy(observation))
+            raw_action = dist.rsample()
+            action = torch.tanh(raw_action)
+            squash_action = torch.tanh(raw_action)
+            action = (squash_action * self.action_scale + self.action_bias)
+
+        return ptu.to_numpy(action)[0]
+
+    def actions_and_logprobs(self, observations: torch.FloatTensor, eps=1e-6):
+
+        if len(observations.shape) > 1:
+            observations = observations
+        else:
+            observations = observations[None]
+
+        if isinstance(observations, np.ndarray):
+            observations = ptu.from_numpy(observations)
+
+        if self.discrete:
+            dist = self(observations)
+            actions = dist.rsample()
+            log_probs = dist.log_prob(actions)
+        else:
+            dist = self(observations)
+            raw_actions = dist.rsample()
+            squash_actions = torch.tanh(raw_actions)
+            actions = squash_actions * self.action_scale + self.action_bias
+            log_probs = dist.log_prob(raw_actions)
+            log_probs -= torch.log(self.action_scale * (1.0-squash_actions.pow(2))+ eps)
+            log_probs = log_probs.sum(dim=-1)
+
+        return actions, log_probs
+
+    def update(self, observations, q_networks):
+        #self.observations = ptu.from_numpy(observations)
+        pass
